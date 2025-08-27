3acea73e2c9574bd08b6fb374853a6a9a83bef16
4ll0w3v1l <53517147+4ll0w3v1l@users.noreply.github.com>
2025-08-27 20:24:17 +0300

    users routers

---
+++ routers/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Request
+from schemas.users import User
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/", response_model=User)
+def create_user(user: User, request: Request):
+    if request.app.state.db:
+        last_id = next(reversed(request.app.state.db))
+        request.app.state.db[last_id + 1] = user.model_dump()
+    else:
+        request.app.state.db[0] = user.model_dump()
+
+    return user
+
+@router.get("/")
+def get_users(request: Request):
+    if request.app.state.db:
+        return request.app.state.db
+    else:
+        return []
